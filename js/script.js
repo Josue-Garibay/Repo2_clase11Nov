21354b1824f94b354832dc734159cc5c70a3c470
Josue Garibay <josueclassfi@gmail.com>
2025-11-11 15:30:44 -0600

    init: Creando repo

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let x = 10;
+x = 1000;
+console.log(x);
+
+//Iteraciones
+const cielo = document.getElementById("cielo");
+
+for(let i = 0; i < 5; i++){
+    cielo.innerHTML += "⭐";
+}
+
+const planetas = ["🌍", "🪐", "🌕"]
+
+for(const planeta of planetas){
+    cielo.innerHTML += planeta;
+}
+
+let n = 0
+while(n<5){
+    cielo.innerHTML += "☁️";
+    n++
+}
+
+let m = 0
+do{
+    cielo.innerHTML += "🚀";
+    m++;
+}while(m<4)
+
+const btnEstrella = document.getElementById("btnEstrella");
+
+btnEstrella.addEventListener("click", ()=> {
+    cielo.innerHTML += "⭐";
+})
